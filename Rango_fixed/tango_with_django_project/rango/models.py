c70b756dafe5741765f33f764cc3c81f0f41183b
Steve Quinn <swquinn@hotmail.co.uk>
2017-02-12 17:38:02 +0000

    End of chapter 9

--- Rango_fixed/tango_with_django_project/rango/models.py
+++ Rango_fixed/tango_with_django_project/rango/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -34,3 +35,17 @@ class Page(models.Model):
 
     def __unicode__(self):
         return self.title
+
+class UserProfile(models.Model):
+    # required line - links UserProfile to user model instance
+    user = models.OneToOneField(User)
+
+    #additional attributes
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.username
+
+    def __unicode__(self):
+        return self.username
